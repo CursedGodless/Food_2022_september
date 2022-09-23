06604abb1736aaf8a9228a8647baa786bfe5d508
Artyom <saaworking@gmail.com>
2022-09-23 19:50:25 +0300

    sending forms

--- js/script.js
+++ js/script.js
@@ -70,3 +70,160 @@ function setZero(num) {
 	}
 }
 
+// Модалка
+
+const modalBtns = document.querySelectorAll('[data-modal]'),
+	modal = document.querySelector('.modal'),
+	modalClose = document.querySelector('.modal__close');
+
+modalBtns.forEach(item => {
+	item.addEventListener('click', openModal)
+})
+
+modal.addEventListener('click', (e) => {
+	const target = e.target;
+	if (target.matches('.modal__close') || target.matches('.modal')) {
+		closeModal();
+	}
+})
+
+document.addEventListener('keydown', (e) => {
+	if (modal.classList.contains('show') && e.code === 'Escape') {
+		closeModal();
+	}
+})
+
+function openModal() {
+	modal.classList.add('show');
+	document.body.style.overflow = 'hidden';
+	clearTimeout(timerId);
+}
+
+function closeModal() {
+	modal.classList.remove('show');
+	document.body.style.overflow = '';
+}
+
+let timerId = setTimeout(openModal, 15000);
+
+function openOnBottom() {
+	if (document.documentElement.scrollHeight - 1 <= document.documentElement.scrollTop + document.documentElement.clientHeight) {
+		openModal();
+		document.removeEventListener('scroll', openOnBottom)
+
+	}
+}
+
+document.addEventListener('scroll', openOnBottom);
+
+
+class MenuCard {
+	constructor(src, alt, subtitle, descr, price, parentSelector, ...classes) {
+		this.src = src;
+		this.alt = alt;
+		this.subtitle = subtitle;
+		this.descr = descr;
+		this.price = price;
+		this.parentSelector = document.querySelector(parentSelector);
+		this.classes = classes.length ? classes : 'menu__item';
+	}
+
+	render() {
+		this.parentSelector.insertAdjacentHTML('beforeend', `
+		<div class=${this.classes}>
+				<img src=${this.src} alt=${this.alt}>
+				<h3 class="menu__item-subtitle">${this.subtitle}</h3>
+				<div class="menu__item-descr"> ${this.descr}
+				</div>
+				<div class="menu__item-divider"></div>
+				<div class="menu__item-price">
+					<div class="menu__item-cost">Цена:</div>
+					<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+				</div>
+		</div>
+		`)
+	}
+}
+
+new MenuCard("img/tabs/vegy.jpg", "vegy", 'Меню "Фитнес"', `Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и
+фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким
+качеством!`, 229, '.menu__field .container').render();
+
+
+const forms = document.querySelectorAll('form');
+
+function postData(form) {
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
+
+		const message = {
+			success: 'Данные успешно отправлены',
+			failure: 'Упс... Что-то пошло не так',
+			loading: 'icons/spinner.svg'
+		}
+
+		const loading = document.createElement('img');
+		loading.style.cssText = `
+		display:block;
+		margin:0 auto;
+		margin-top: 15px;
+		`
+		loading.src = message.loading;
+
+		const prevDialog = modal.querySelector('.modal__dialog')
+
+		const formData = new FormData(form);
+
+		function showThanksModal(message, ms = 3000) {
+
+			prevDialog.classList.add('hide')
+			openModal();
+			loading.remove();
+			const statusMessage = document.createElement('div');
+			statusMessage.classList.add('modal__dialog');
+			statusMessage.innerHTML =
+				`
+						<div class="modal__content">
+							<div class="modal__close">&times;</div>
+							<div class="modal__title">${message}</div>
+						</div>
+				`
+
+			modal.append(statusMessage);
+			setTimeout(() => statusMessage.remove(), ms)
+		}
+
+		form.insertAdjacentElement('afterend', loading);
+
+		fetch('server.php', {
+			body: JSON.stringify(Object.fromEntries(formData.entries())),
+			method: 'POST',
+			headers: { 'Content-type': 'application/json' }
+		})
+			.then(response => {
+				if (response.ok) {
+					return response.text()
+				} else {
+					throw new Error('error');
+				}
+			})
+			.then(data => {
+				console.log('sadadad');
+				showThanksModal(message.success);
+			})
+			.catch(() => {
+				showThanksModal(message.failure)
+			})
+			.finally(() => {
+				setTimeout(() => {
+					closeModal();
+					prevDialog.classList.remove('hide')
+					form.reset()
+				}, 3000);
+			})
+	})
+}
+
+forms.forEach(item => {
+	postData(item)
+})
